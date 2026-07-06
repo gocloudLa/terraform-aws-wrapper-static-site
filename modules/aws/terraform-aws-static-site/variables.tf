e16fe78f48e954ac7724e225278f8fe70dbabf96
Ignacio Bisio <ibisio@gocloud.la>
2026-07-06 13:20:03 -0300

    feat(cloudfront): add custom name for origin access

--- modules/aws/terraform-aws-static-site/variables.tf
+++ modules/aws/terraform-aws-static-site/variables.tf
@@ -158,6 +158,18 @@ variable "acm_certificate_arn" {
   type = string
 }
 
+variable "origin_access_control_name" {
+  description = "Name of the Origin Access Control for the CloudFront distribution."
+  type        = string
+  default     = null
+}
+
+variable "origin_access_control_key_name" {
+  description = "Name of the Origin Access Control signing key for the CloudFront distribution."
+  type        = string
+  default     = null
+}
+
 /*----------------------------------------------------------------------*/
 /* Bucket | Variable Definition                                    */
 /*----------------------------------------------------------------------*/
